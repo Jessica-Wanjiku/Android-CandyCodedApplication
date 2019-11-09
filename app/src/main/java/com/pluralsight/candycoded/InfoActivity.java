15e1858cf955711299323edd00744cc717728917
Jessica-wanjiku <wanjikuwanjiru24@gmail.com>
2019-11-09 08:42:49 +0300

    first commit add intent

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -33,8 +33,8 @@ public class InfoActivity extends AppCompatActivity {
         Intent mapIntent = new Intent (Intent.ACTION_VIEW, uriAddress);
         mapIntent.setPackage("com.goggle.android.apps.maps");
 
-        if(mapIntent.resolveActivity(getPackageManager()) !=null) {
-        startActivity(mapIntent);
+        if(mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
         }
 
     }
